78e2ed9290abb91b00e3c2d6ff384261b39e0e02
turkaka <turkaka33@hotmail.com>
2020-11-10 10:51:25 +0100

    plugin

--- funktion.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-
-add_filter("the_content","beregning_system_Function");
-function beregning_system_Function($content){
-  $skriv = "Everything will be fine!";
-  return $content.$skriv;
-}
--- vinduespolering.php
+++ vinduespolering.php
@@ -36,10 +36,14 @@ add_action("admin_menu","PluginAdminMenu");
 function PluginAdminMenu(){
 
   add_menu_page("Beregning System", "Beregning System", "manage_options","beregning-settings","pluginadminfunctions","dashicons-grid-view", NULL);
-  add_submenu_page("beregning-settings","Vinduespolering","Vinduespolering","manage_options","vinduespolering",array("beregning-settings","vinduespolering"));
+  add_submenu_page("beregning-settings","Vinduespolering","Vinduespolering","manage_options","vinduespolering");
 
 }
 
+function menumuz() {
+add_menu_page("Wordpress Sistem Destek Eklentisi","SİSDESTEK","manage_options","ana_menu",array("destek","anasayfa"),"dashicons-shield",99);
+
+}
 
 function pluginadminfunctions (){
   if($_POST["action"]=="update"){
@@ -61,99 +65,30 @@ function pluginadminfunctions (){
   }
 ?>
 <h1>Beregning System </h1>
-
-<form action="/action_page.php">
-
-  <input type="submit" value="Submit">
-</form>
 <form action="" method="post">
   <?php wp_nonce_field('testplugin','testplugin'); ?>
-  <label for="wname">Hvad er din postnummer?</label>
+
+  <label for="">Beregn system</label><br>
   <input type="text" name="postnr" value="" placeholder="Postnummer">
+  <input type="text" name="windows" value="" placeholder="Antal Vinduer">
+  <input type="text" name="doors" value="" placeholder="Antal Dør">
 
-  <label for="wname">Hvor mange vinduer har du?</label>
-    <br>
-   <select id="window" name="windows">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-  </select>
-
-  <label for="wname">Hvad er din vinduestype type`?</label>
-  <select id="windowtype" name="windowtype">
-   <option value="etfagsvindue">etfagsvindue</option>
-   <option value="tofagsvindue">tofagsvindue</option>
-   <option value="trefagsvindue">trefagsvindue</option>
-   <option value="firefagsvindue">firefagsvindue</option>
-   <option value="femfagsvindue">femfagsvindue</option>
-   <option value="seksfagsvindue">seksfagsvindue</option>
- </select>
-
-    <br>
-
-  <label for="dname">Hvor mange glasdøre har du?</label><br>
-   <select id="gdoor" name="gdoors">
-    <option value="0">0</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-  </select>
-
-    <br>
-
-  <label for="bname">Boligtype</label><br>
-  <select id="hustype" name="hustype">
-    <option value="villa1">Villa - et plan</option>
-    <option value="villa2">Villa - to plan</option>
-    <option value="lejlighed">Lejlighed</option>
-    <option value="Rækkehus">Rækkehus</option>
-  </select>
-    <br>
-    <label for="bname">Boigareal</label><br>
-  <input type="text" name="antal" value="" placeholder="Bolig Areal">
-  <br>
-  <label for="bname">Boligtype</label><br>
-  <select id="hustype" name="hustype">
-    <option value="villa1">Villa - et plan</option>
-    <option value="villa2">Villa - to plan</option>
-    <option value="lejlighed">Lejlighed</option>
-    <option value="Rækkehus">Rækkehus</option>
-  </select>  <br>
-
-    <label for="bname">Hvad tit ville du gerne rengøring</label><br>
-    <select id="ofte" name="ofte">
-      <option value="engang">En gang om måneden</option>
-      <option value="togang">To gang om måneden</option>
-      <option value="hveruge">Hver uge</option>
-    </select>  <br>
-
+  <input type="text" name="ofte" value="" placeholder="Hvor mange gang">
+  <input type="hidden" name="action" value="update" placeholder="">
   <input type="submit" value="send">
 
 </form>
 <br>
 <h1>Beregning Tabel fra Database</h1>
 <?php
-
-}
-
-?>
-<h1>Beregning Tabel fra Database</h1>
-<?php
-function pluginfunctions (){
 global $wpdb;
 $list = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}tableberegn");
 foreach ($list as $beregnlist){
   echo "Postnummer: ".$beregnlist->postnr." Antal vinduer: ".$beregnlist->windows." Antal dør: ".$beregnlist->doors." Area: ".$beregnlist->antal." Vinduer Type: ".$beregnlist->type." Hustype: ".$beregnlist->hustype." Hvor mange gang: ".$beregnlist->ofte;
   echo '<br>';
 }
+}
+
 // Register a new shortcode: [cr_custom_registration]
 add_shortcode( 'kasim', 'custom_registration_shortcode' );
 
