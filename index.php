f9be91fbce85b57bc6b84ae144e57c02bf010e1b
Benjamin Porsgaard <benjamin_porsgaard@hotmail.com>
2020-12-14 10:28:14 +0100

    Index side lavet - Admin panel oprettet til WP

--- index.php
+++ index.php
@@ -9,27 +9,35 @@ Author URI: www.kasim.dk/beregn
 License: GNU
 */
 
+
 global $wpdb;
 
-if ($wpdb->get_var("SHOW TABLES LIKE '{$wpdb->prefix}tableberegn'") != $wpdb->prefix . 'tableberegn') {
-      $wpdb->query("CREATE TABLE {$wpdb->prefix}tableberegn(
-        id integer not null auto_increment,
-        postnr TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        windows TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        doors TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        antal TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        type TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        hustype TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        ofte TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        PRIMARY KEY (id)
-        );");
-}
 
 add_action('admin_enqueue_scripts',"addCss");
 
 function addCss() {
    wp_enqueue_style('prefix-style',plugins_url('css/style.css',__FILE__));
 }
+
+include 'frontpage.php';
+
+
+if ($wpdb->get_var("SHOW TABLES LIKE '{$wpdb->prefix}tableberegn'") != $wpdb->prefix . 'tableberegn') {
+      $wpdb->query("CREATE TABLE {$wpdb->prefix}tableberegn(
+        id integer not null auto_increment,
+        postnr TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+        windows9 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+        windows19 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+        windows29 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+        villa1 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+        villa2 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+        villa3 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+        PRIMARY KEY (id)
+        );");
+}
+
+
+
 
 add_action("admin_menu","PluginAdminMenu");
 
@@ -41,6 +49,7 @@ function PluginAdminMenu(){
 }
 
 
+
 function pluginadminfunctions (){
   if($_POST["action"]=="update"){
     if(!isset($_POST["testplugin"]) || ! wp_verify_nonce($_POST["testplugin"], 'testplugin')){
@@ -48,121 +57,66 @@ function pluginadminfunctions (){
     }else {
       global $wpdb;
       $postnr = sanitize_text_field($_POST["postnr"]);
-      $windows = sanitize_text_field($_POST["windows"]);
-      $doors = sanitize_text_field($_POST["doors"]);
-      $type = sanitize_text_field($_POST["type"]);
-      $hustype = sanitize_text_field($_POST["hustype"]);
-      $ofte = sanitize_text_field($_POST["ofte"]);
+      $windows9 = sanitize_text_field($_POST["windows9"]);
+      $windows19 = sanitize_text_field($_POST["windows19"]);
+      $windows29 = sanitize_text_field($_POST["windows29"]);
+      $villa1 = sanitize_text_field($_POST["villa1"]);
+      $villa2 = sanitize_text_field($_POST["villa2"]);
+      $villa3 = sanitize_text_field($_POST["villa3"]);
 
-      $wpdb->insert("{$wpdb->prefix}tableberegn",array("postnr"=>$postnr,"windows"=>$windows,"doors"=>$doors,"antal"=>$antal,"type"=>$type,"hustype"=>$hustype,"ofte"=>$ofte));
+      $wpdb->insert("{$wpdb->prefix}tableberegn",array("postnr"=>$postnr,"windows9"=>$windows9,"windows19"=>$windows19,"windows29"=>$windows29,"villa1"=>$villa1,"villa2"=>$villa2,"villa3"=>$villa3));
       echo '<div class="updated">Setting updated</div>';
     }
 
   }
 ?>
+<div class="beregn">
 <h1>Beregning System </h1>
 
-<form action="/action_page.php">
+<?php
+global $wpdb;
+$list = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}tableberegn ORDER BY id DESC LIMIT 1");
+foreach ($list as $beregnlist){
 
-  <input type="submit" value="Submit">
-</form>
+?>
+
+
 <form action="" method="post">
+
   <?php wp_nonce_field('testplugin','testplugin'); ?>
-  <label for="wname">Hvad er din postnummer?</label>
-  <input type="text" name="postnr" value="" placeholder="Postnummer">
 
-  <label for="wname">Hvor mange vinduer har du?</label>
-    <br>
-   <select id="window" name="windows">
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-  </select>
+  <label for="wname">Hvilken post nummer vil du gerne arbejde?</label>
+  <textarea name="postnr" >
+  <?php echo $beregnlist->postnr;  ?>
 
-  <label for="wname">Hvad er din vinduestype type`?</label>
-  <select id="windowtype" name="windowtype">
-   <option value="etfagsvindue">etfagsvindue</option>
-   <option value="tofagsvindue">tofagsvindue</option>
-   <option value="trefagsvindue">trefagsvindue</option>
-   <option value="firefagsvindue">firefagsvindue</option>
-   <option value="femfagsvindue">femfagsvindue</option>
-   <option value="seksfagsvindue">seksfagsvindue</option>
- </select>
+        </textarea>
 
-    <br>
+  <label for="wname">Vinduers priser</label>
+    <p>1-9 Vinduer : <input type="text" name="windows9" value="<?php echo $beregnlist->windows9;  ?>"></p>
 
-  <label for="dname">Hvor mange glasdøre har du?</label><br>
-   <select id="gdoor" name="gdoors">
-    <option value="0">0</option>
-    <option value="1">1</option>
-    <option value="2">2</option>
-    <option value="3">3</option>
-    <option value="4">4</option>
-    <option value="5">5</option>
-    <option value="6">6</option>
-    <option value="7">7</option>
-    <option value="8">8</option>
-  </select>
+    <p>10-19 Vinduer: <input type="text" name="windows19" value="<?php echo $beregnlist->windows19;  ?>"></p>
 
-    <br>
+    <p>20-29 Vinduer: <input type="text" name="windows29" value="<?php echo $beregnlist->windows29;  ?>"></p>
 
-  <label for="bname">Boligtype</label><br>
-  <select id="hustype" name="hustype">
-    <option value="villa1">Villa - et plan</option>
-    <option value="villa2">Villa - to plan</option>
-    <option value="lejlighed">Lejlighed</option>
-    <option value="Rækkehus">Rækkehus</option>
-  </select>
-    <br>
-    <label for="bname">Boigareal</label><br>
-  <input type="text" name="antal" value="" placeholder="Bolig Areal">
-  <br>
-  <label for="bname">Boligtype</label><br>
-  <select id="hustype" name="hustype">
-    <option value="villa1">Villa - et plan</option>
-    <option value="villa2">Villa - to plan</option>
-    <option value="lejlighed">Lejlighed</option>
-    <option value="Rækkehus">Rækkehus</option>
-  </select>  <br>
+<br>
+  <label for="bname">Boligtype </label>
+  <p>Eksta betaling. Det sætter over vinduers priser</p>
 
-    <label for="bname">Hvad tit ville du gerne rengøring</label><br>
-    <select id="ofte" name="ofte">
-      <option value="engang">En gang om måneden</option>
-      <option value="togang">To gang om måneden</option>
-      <option value="hveruge">Hver uge</option>
-    </select>  <br>
+  <p>Et plan: <input type="text" name="villa1" value="<?php echo $beregnlist->villa1;  ?>"></p>
+
+  <p>To plan: <input type="text" name="villa2" value="<?php echo $beregnlist->villa2;  ?>"></p>
+
+  <p>Tre plan: <input type="text" name="villa3" value="<?php echo $beregnlist->villa3;  ?>"></p>
+
+  <input type="hidden" name="action" value="update" placeholder="">
 
   <input type="submit" value="send">
 
 </form>
 <br>
-<h1>Beregning Tabel fra Database</h1>
+</div>
 <?php
-
 }
-
-?>
-<h1>Beregning Tabel fra Database</h1>
-<?php
-function pluginfunctions (){
-global $wpdb;
-$list = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}tableberegn");
-foreach ($list as $beregnlist){
-  echo "Postnummer: ".$beregnlist->postnr." Antal vinduer: ".$beregnlist->windows." Antal dør: ".$beregnlist->doors." Area: ".$beregnlist->antal." Vinduer Type: ".$beregnlist->type." Hustype: ".$beregnlist->hustype." Hvor mange gang: ".$beregnlist->ofte;
-  echo '<br>';
 }
-// Register a new shortcode: [cr_custom_registration]
-add_shortcode( 'kasim', 'custom_registration_shortcode' );
-
-// The callback function that will replace [book]
-function custom_registration_shortcode() {
-    ob_start();
-    pluginadminfunctions();
-    return ob_get_clean();
-}
-
 
 ?>
