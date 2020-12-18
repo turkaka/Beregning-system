5a67fd761b3d72d5adbdce194efb232c25e97014
turkaka <turkaka33@hotmail.com>
2020-12-18 10:55:12 +0100

    kasim
    sids

--- index.php
+++ index.php
@@ -1,126 +1,209 @@
 <?php
-/*
-Plugin Name: Rengøring beregning system
-Plugin URI: www.kasim.dk/beregn
-Description: Beregning system til Rengøring firmaer.
-Version: 4.0
-Author: Kasim Kara, Benjamin Fabricius Porsgaard, Mark Jørgensen
-Author URI: www.kasim.dk/beregn
-License: GNU
-*/
 
 
-global $wpdb;
-
-    add_action('wp_enqueue_scripts',"addCss");
-    add_action('admin_enqueue_scripts',"addCss");
+function pluginindexfunctions (){
 
 
-function addCss() {
-   wp_enqueue_style('prefix-style',plugins_url('css/style.css',__FILE__));
-}
-
-
-include('frontpage.php');
-
-
-if ($wpdb->get_var("SHOW TABLES LIKE '{$wpdb->prefix}tableberegn'") != $wpdb->prefix . 'tableberegn') {
-      $wpdb->query("CREATE TABLE {$wpdb->prefix}tableberegn(
-        id integer not null auto_increment,
-        postnr TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        windows9 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        windows19 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        windows29 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        villa1 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        villa2 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        villa3 TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-        PRIMARY KEY (id)
-        );");
-    $wpdb->insert("{$wpdb->prefix}tableberegn",array("postnr"=>"Postnummer","windows9"=>"intaste priser","windows19"=>"intaste priser","windows29"=>"intaste priser","villa1"=>"intaste priser","villa2"=>"intaste priser","villa3"=>"intaste priser"));
-
-}
-
-add_action("admin_menu","PluginAdminMenu");
-
-function PluginAdminMenu(){
-
-  add_menu_page("Beregning System", "Beregning System", "manage_options","beregning-settings","pluginadminfunctions","dashicons-grid-view", NULL);
-
-}
-
-
-
-function pluginadminfunctions (){
-  if($_POST["action"]=="update"){
-    if(!isset($_POST["testplugin"]) || ! wp_verify_nonce($_POST["testplugin"], 'testplugin')){
-      echo "Error";
-    }else {
-      global $wpdb;
-      $postnr = sanitize_text_field($_POST["postnr"]);
-      $windows9 = sanitize_text_field($_POST["windows9"]);
-      $windows19 = sanitize_text_field($_POST["windows19"]);
-      $windows29 = sanitize_text_field($_POST["windows29"]);
-      $villa1 = sanitize_text_field($_POST["villa1"]);
-      $villa2 = sanitize_text_field($_POST["villa2"]);
-      $villa3 = sanitize_text_field($_POST["villa3"]);
-
-
-        $wpdb->update("{$wpdb->prefix}tableberegn", array("postnr"=>$postnr,"windows9"=>$windows9,"windows19"=>$windows19,"windows29"=>$windows29,"villa1"=>$villa1,"villa2"=>$villa2,"villa3"=>$villa3), array('id' => '1'));
-
-      echo '<div class="updated">Setting updated</div>';
-    }
-
-  }
 ?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 <div class="beregn">
-<h1>Beregningssystem </h1>
-<p>Admin Panel</p>
-<?php
-global $wpdb;
-$list = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}tableberegn ORDER BY id DESC LIMIT 1");
-foreach ($list as $beregnlist){
 
-?>
+<h1>Beregningssystem </h1>
 
 
 <form action="" method="post">
-
   <?php wp_nonce_field('testplugin','testplugin'); ?>
+  <label for="wname">Hvad er dit postnummer?</label>
+  <input type="text" name="postnr" value="" placeholder="Postnummer">
 
-  <label for="wname">Hvilket postnummer vil du gerne arbejde i?</label>
-    <label for="info">Indtast postnummer eksempel (5000, 4200, 7450)</label>
+  <label for="wname">Hvor mange vinduer har du?</label>
+   <select id="window" name="windows">
+    <option value="9">1-9</option>
+    <option value="19">10-19</option>
+    <option value="29">20-29</option>
+  </select><br><br>
+  <label for="bname">Boligtype</label>
+  <select id="hustype" name="hustype">
+    <option value="1">Et plan</option>
+    <option value="2">To plan</option>
+    <option value="3">Tre plan</option>
+  </select><br><br>
 
-  <textarea name="postnr" >
-  <?php echo $beregnlist->postnr;  ?>
+  <input type="submit" value="send">
 
-        </textarea>
-
-  <label for="wname">Vinduernes priser</label>
-    <p>1-9 Vinduer : <input type="text" name="windows9" value="<?php echo $beregnlist->windows9;  ?>"></p>
-
-    <p>10-19 Vinduer: <input type="text" name="windows19" value="<?php echo $beregnlist->windows19;  ?>"></p>
-
-    <p>20-29 Vinduer: <input type="text" name="windows29" value="<?php echo $beregnlist->windows29;  ?>"></p>
-
+</form>
 <br>
-  <label for="bname">Boligtype </label>
-  <p>Eksta betaling for forskellige plan.</p>
 
-  <p>Et plan: <input type="text" name="villa1" value="<?php echo $beregnlist->villa1;  ?>"></p>
 
-  <p>To plan: <input type="text" name="villa2" value="<?php echo $beregnlist->villa2;  ?>"></p>
+<?php
 
-  <p>Tre plan: <input type="text" name="villa3" value="<?php echo $beregnlist->villa3;  ?>"></p>
+global $wpdb;
+$list = $wpdb->get_results("SELECT * FROM {$wpdb->prefix}tableberegn ORDER BY id DESC LIMIT 1");
 
-  <input type="hidden" name="action" value="update" placeholder="">
+foreach ($list as $beregnlist){
+
+    $postnummer =  $beregnlist->postnr;
+
+    $allowed_postal_codes = explode(',', $postnummer);
+
+
+
+if(isset($_POST['postnr'])) { // har vi sendt form
+
+  $postnr = $_POST["postnr"];
+  $windows = $_POST["windows"];
+  $hustype = $_POST["hustype"];
+  $tilbud=' <br> <br> <button id="hide">Få tilbud</button> <button id="hide1">Bestil Rengøring</button>';
+
+
+      if(in_array($postnr, $allowed_postal_codes)) { // info er korrekt
+
+         session_start();
+
+         $_SESSION['postnr'] = $postnr;
+          if ($windows == 9 && $hustype == 1) {
+            $a = $beregnlist->windows9;
+            $b= $beregnlist->villa1;
+            echo $a + $b.' kr.';
+            echo $tilbud;
+
+
+        }elseif ($windows == 9 && $hustype == 2 ) {
+          $a = $beregnlist->windows9;
+          $b= $beregnlist->villa2;
+          echo $a + $b.' kr.';
+        echo $tilbud;
+
+        }elseif ($windows == 9 && $hustype == 3 ) {
+          $a = $beregnlist->windows9;
+          $b= $beregnlist->villa3;
+          echo $a + $b.' kr.';
+          echo $tilbud;
+
+        }elseif ($windows == 19 && $hustype == 1 ) {
+          $a = $beregnlist->windows19;
+          $b= $beregnlist->villa1;
+          echo $a + $b.' kr.';
+          echo $tilbud;
+
+        }elseif ($windows == 19 && $hustype == 2 ) {
+          $a = $beregnlist->windows19;
+          $b= $beregnlist->villa2;
+          echo $a + $b.' kr.';
+          echo $tilbud;
+
+        }elseif ($windows == 19 && $hustype == 3 ) {
+          $a = $beregnlist->windows19;
+          $b= $beregnlist->villa3;
+          echo $a + $b.' kr.';
+          echo $tilbud;
+
+        }elseif ($windows == 29 && $hustype == 1 ) {
+          $a = $beregnlist->windows29;
+          $b= $beregnlist->villa1;
+          echo $a + $b.' kr.';
+         echo $tilbud;
+
+        }elseif ($windows == 29 && $hustype == 2 ) {
+          $a = $beregnlist->windows29;
+          $b= $beregnlist->villa2;
+          echo $a + $b.' kr.';
+         echo $tilbud;
+
+
+        }elseif ($windows == 29 && $hustype == 3 ) {
+          $a = $beregnlist->windows29;
+          $b= $beregnlist->villa3;
+          echo $a + $b.' kr.';
+         echo $tilbud;
+
+        }
+             else{
+        echo 'Prisen er 5000kr';
+        }
+
+      } else {
+         echo 'Vi kan desværre ikke levere vinduespolering uden for Odense';
+
+      }
+}
+
+}
+           ?>
+<div class="formul">
+
+<div id="tilbudDIV">
+
+
+<form action="" method="post">
+  <?php wp_nonce_field('testplugin','testplugin'); ?>
+  <label for="wname">Få tilbud</label>
+
+   <input type="text" name="email" value="" placeholder="Dit navn">
+
+  <input type="text" name="email" value="" placeholder="Din Email">
+
 
   <input type="submit" value="send">
 
 </form>
 <br>
 </div>
+<div id="bestilDIV">
+
+
+<form action="" method="post">
+  <?php wp_nonce_field('testplugin','testplugin'); ?>
+  <label for="wname">Bestil Rengøring</label>
+
+   <input type="text" name="navn" value="" placeholder="Dit navn">
+
+  <input type="text" name="email" value="" placeholder="Din Email">
+
+    <input type="text" name="adress" value="" placeholder="Din Adress">
+
+  <input type="text" name="telefon" value="" placeholder="Din Telefon nummer">
+
+  <p><input type="checkbox" id="person1" name="person1" value="Persondata"> Ja, jeg accepterer at du kan kontakt med mig</p>
+
+  <input type="submit" value="send">
+
+</form>
+<br>
+</div>
+</div>
+
+</div>
+
+<script>
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("#tilbudDIV").toggle();
+  });
+});
+$(document).ready(function(){
+  $("#hide1").click(function(){
+    $("#bestilDIV").toggle();
+  });
+});
+
+</script>
+
 <?php
+
+
 }
+
+// Register a new shortcode: [beregn]
+add_shortcode( 'beregn', 'custom_registration_shortcode' );
+
+// The callback function that will replace [beregn]
+function custom_registration_shortcode() {
+    ob_start();
+    pluginindexfunctions();
+    return ob_get_clean();
 }
 
 
